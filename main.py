104a6cc1b856f7fc0a772dc8dad5fbe097280f92
Lucas <lucasribeirostrefezza@gmail.com>
2026-04-21 09:40:52 -0300

    refactor: optimize irrigation logic with weather-based precipitation check and update sensor calibration

--- main.py
+++ main.py
@@ -21,32 +21,20 @@ sensor_dht = dht.DHT22(Pin(14))
 sensor_ldr = ADC(Pin(34))
 
 #Define a resolução para 12 bits (0-4095)
-sensor_ldr.width(ADC.WIDTH_12BIT)
+# sensor_ldr.width(ADC.WIDTH_12BIT)
 
 #Configuração do relé.
-rele1 = Pin(23, Pin.OUT)
+rele = Pin(23, Pin.OUT)
 
 #Função para converter o valor analógico (32-4095) em uma proporção de PH (0-14)
 def analog_to_ph(x):
     #Definindo os limites
     min_origem, max_origem = 32, 4095
-    min_destino, max_destino = 0, 14.1
+    min_destino, max_destino = 14, 0
     #Garante que o valor esteja dentro do intervalo de entrada antes do cálculo
     x = max(min_origem, min(x, max_origem))
     resultado = (x - min_origem) * (max_destino - min_destino) / (max_origem - min_origem) + min_destino
     return resultado
-
-#Função para buscar dados de clima na API do Google
-def fetch_weather():
-    url = "https://weather.googleapis.com/v1/forecast/days:lookup?key=AIzaSyB8PR0kUmJb2f_DuBGuAxdTE6sK6QcLrt8&location.latitude=37.4220&location.longitude=-122.0841"
-    try:
-        print("Consultando a API de clima...\n")
-        response = urequests.get(url)
-        print(f"Status da resposta:{response.status_code}\n")
-        print(f"Dados:\n{response.text}\n") # Descomente para ver os dados retornados
-        response.close()
-    except Exception as e:
-        print(f"Erro na requisição:{e}\n")
 
 #Configuração do Wi-Fi (Wokwi-GUEST é a rede padrão do simulador Wokwi)
 print("Conectando ao Wi-Fi", end="")
@@ -57,6 +45,21 @@ while not sta_if.isconnected():
     print(".", end="")
     sleep(0.1)
 print(" Conectado com sucesso!\n")
+
+#Função para buscar dados de clima na API do Google
+def fetch_weather():
+    url = "https://weather.googleapis.com/v1/forecast/days:lookup?key=AIzaSyB8PR0kUmJb2f_DuBGuAxdTE6sK6QcLrt8&location.latitude=23.5614&location.longitude=46.6564"
+    try:
+        print("Consultando a API de clima...")
+        response = urequests.get(url)
+        print(f"Status da resposta:{response.status_code}")
+        # print(f"Dados:\n{response.text}\n") # Descomente para ver os dados retornados
+        precipitation = response.json()['forecastDays'][0]['daytimeForecast']['precipitation']['probability']['percent']
+        response.close()
+    except Exception as e:
+        print(f"Erro na requisição:{e}\n")
+        precipitation = 0 #Caso de erro, assume 0%
+    return precipitation
 
 #Loop principal.
 while True:
@@ -81,18 +84,23 @@ while True:
         ph = sensor_ldr.read();
         ph =  analog_to_ph(ph);
 
-        print("Umidade do solo: {:.1f}%".format(h))
-        print("PH: {:.1f}%\n".format(ph))
+        print(f"Umidade do solo: {h:.1f}%")
+        print(f"PH: {ph:.1f}%")
+        
+        #Busca dados de clima.
+        precipitation = fetch_weather()
+        print(f"Precipitação: {precipitation}%\n")
 
         #Relé: Irrigação do Solo (Bomba d'água).
-        if h < 25: 
-            rele1.value(1)
-        else:
-            rele1.value(0)
-
-        fetch_weather()
+        if precipitation < 10: #Se houver previsão de chuva menor que 10%
+            if h < 25: #Se houver umidade menor que 25%
+                rele.value(1) #Liga a bomba
+            else:
+                rele.value(0) #Desliga a bomba
+        else: #Se houver previsão de chuva maior ou igual a 10%
+            rele.value(0) #Desliga a bomba
 
     except OSError as e:
         print("Falha na leitura dos sensores. Verifique as conexões.")
 
-    sleep(5.1) #Intervalo para a leitura dos sensores.
+    sleep(0.1) #Intervalo para a leitura dos sensores.
